4069c8c9b8f1a3a376586d15cc103bcbc7b8cd37
Guillaume <crystalnoir63@gmail.com>
2015-03-18 16:43:31 +0100

    Ajout de deux fichiers pour la classe utilisateur

---
+++ app/modele/User.php
@@ -0,0 +1,60 @@
+<?php
+
+//**Class User**//
+class User {
+  //Attributs
+  private $id;
+  private $login;
+  private $email;
+  private $password;
+  private $role;
+
+  //Getters
+  public function getId() {
+    return $this->id;
+  }
+
+  public function getLogin() {
+    return $this->login;
+  }
+
+  public function getPassword() {
+    return $this->password;
+  }
+
+  public function getEmail() {
+    return $this->email;
+  }
+
+  public function getRole() {
+    return $this->role;
+  }
+
+  //Setters
+  public function setId($id) {
+    $this->id = $id;
+  }
+
+  public function setLogin($login) {
+    $this->login = $login;
+  }
+
+  public function setPassword($password) {
+    $this->password = $password;
+  }
+
+  public function setEmail($email) {
+    $this->email = $email;
+  }
+
+  public function setRole($role) {
+    $this->role = $role;
+  }
+
+}
+
+
+
+
+
+?>
