f09faeabd6f9cc60f866cfe557c9237447259798
StefNoMan <stephan.audonnet@live.fr>
2015-03-18 21:14:47 +0100

    refresh de la page toutes les 15sec

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,12 @@
 
-// durée d'un interval entre 2 recherches de nouveaux messages
-var interval = 2000;
 
 $(function(){
+	
+	// durée d'un interval entre 2 recherches de nouveaux messages
+	var interval = 15000;
 
-	$('#chat-form').on('submit', function() {
-		event.preventDefault();
-		var message = $("#message").val();
-		postMessage(message);
-	})
-
-
-	// lance la fonction de récupération des nouveaux messages toutes les "interval" secondes
-	// window.setInterval(getMessages, interval);
+	window.setInterval(function(){
+		location.reload();
+	}, interval);
 
 });
