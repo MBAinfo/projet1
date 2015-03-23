73e937fb02e0ec5591aabb639354c357a6c4afd0
StefNoMan <stephan.audonnet@live.fr>
2015-03-23 10:32:16 +0100

    added config

---
+++ include/config.exemple.php
@@ -0,0 +1,17 @@
+<?php 
+
+	$config = array(
+		'app' => array(
+			'debug_mode' => true,
+			'keywords' => array(
+				'#todo',
+				'#document',
+			)
+		),
+		'db' => array(
+			'hostname' => "xxxxx",
+			'user'     => "xxxxx",
+			'password' => "xxxxx",
+			'nom_base_donnees' => "xxxxx",
+		),
+	);
